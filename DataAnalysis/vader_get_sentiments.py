24c1172aedad422f9859b3731b2ab4b9551e34a3
amathur-19301666 <amathur@tcd.ie>
2020-04-13 14:31:46 +0100

    updated code

--- DataAnalysis/vader_get_sentiments.py
+++ DataAnalysis/vader_get_sentiments.py
@@ -2,11 +2,16 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import json
 from os import listdir
 from os.path import isfile, join
+import plotly.graph_objects as go
 
 mypath = "releases2"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-print(onlyfiles)
+#print(onlyfiles)
+
+positive_releases = 0
+
+negative_releases = 0
 
 for file in onlyfiles:
 
@@ -19,11 +24,22 @@ for file in onlyfiles:
     for i in range(len(data)):
         sentence = data[i]['content']
         vs = analyzer.polarity_scores(sentence)
+        pos = vs['pos']
+        neg = vs['neg']
+        if pos > neg:
+            positive_releases += 1
+        else:
+            negative_releases += 1
         to_append = {"intent":vs}
         data[i].update(to_append)
-    print(data)
+    #print(data)
     
     new_path = "releases3_with_sentiment/" + file
-    print(new_path)
+    #print(new_path)
     with open(new_path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
+        
+
+press_releases = ['Positive', 'Negative']
+fig = go.Figure([go.Bar(x = press_releases, y=[positive_releases, negative_releases], color='lifeExp')])
+fig.write_html("file.html")
